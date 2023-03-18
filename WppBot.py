c579bd6eb63e8a8af66d5341fd756da89578b7bd
Ulysses-Alv <ulises.j.alvarenga@gmail.com>
2023-03-17 23:11:06 -0300

    small fix

--- WppBot.py
+++ WppBot.py
@@ -1,18 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchWindowException
-from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import InvalidArgumentException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
-import os
 import time
-import math
 import datetime
-import argparse
 import usageMemory
 import json
 import sys
